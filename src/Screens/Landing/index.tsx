7dc00ee08616e5179d512fd5811ee63384c559f2
James <jxbromley@ualr.edu>
2021-05-12 20:46:17 -0500

    Bootstrapping more packages etc. Grid demo system on landing page.

--- src/Screens/Landing/index.tsx
+++ src/Screens/Landing/index.tsx
@@ -1,23 +1,51 @@
-import React, { FC } from 'react';
-import { makeStyles, Theme } from '@material-ui/core/styles';
-import { Box } from '@material-ui/core';
+import React, { FC } from "react";
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import { Box, createStyles, Grid, Paper } from "@material-ui/core";
 
-export interface LandingScreenProps {
-
-}
+export interface LandingScreenProps {}
 
 const LandingScreen: FC<LandingScreenProps> = (props) => {
-    const classes = useStyles();
+  const classes = useStyles(props);
 
-    return (
-        <Box>
-            Welcome to React-material UI
-        </Box>
-    )
-}
-
-const useStyles = makeStyles<Theme, any>((theme) => ({
-
-}));
+  return (
+    <Box>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+    },
+  })
+);
 
 export default LandingScreen;
